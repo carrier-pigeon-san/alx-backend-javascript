4c1be5165c6687797db8106a89afab23b43deacc
Sandy Obare <obaresandy@gmail.com>
2024-11-24 01:19:46 +0300

    10-loops.js update: for loop syntax update3

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   const array2 = [...array];
-  for (const element of array2) {
+  for (const element of array) {
     const index = array.indexOf(element);
     array2[index] = appendString + element;
   }
