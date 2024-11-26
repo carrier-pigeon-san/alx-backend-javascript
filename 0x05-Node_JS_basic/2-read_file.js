d9d9635e28cb5039a126bf6f4b4c9ed49c922fa1
Sandy Obare <obaresandy@gmail.com>
2024-11-26 19:05:27 +0300

    2-read_file.js update: logic update

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,29 +1,31 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  try {
-    const data = fs.readFileSync(path, { encoding: 'utf8' }).split('\n');
-    let students = 0;
-    let studentsCS = 0;
-    let studentsSWE = 0;
-    let studentListCS = '';
-    let studentListSWE = '';
-    for (let i = 1; i < data.length; i += 1) {
-      if (data[i].split(',')[3] === 'CS') {
-        studentListCS += `${data[i].split(',')[0]}, `;
+  if (!fs.existsSync(path)) {
+    throw new Error('Cannot load the database');
+  }
+  const data = fs.readFileSync(path, 'utf8').split('\n');
+  let students = 0;
+  let studentsCS = 0;
+  let studentsSWE = 0;
+  const studentsCSList = [];
+  const studentsSWEList = [];
+  for (let i = 1; i < data.length; i += 1) {
+    if (data[i]) {
+      const student = data[i].split(',');
+      if (student[3] === 'CS') {
         studentsCS += 1;
-      } else if (data[i].split(',')[3] === 'SWE') {
-        studentListSWE += `${data[i].split(',')[0]}, `;
+        studentsCSList.push(student[0]);
+      } else if (student[3] === 'SWE') {
         studentsSWE += 1;
+        studentsSWEList.push(student[0]);
       }
       students += 1;
     }
-    console.log(`Number of students: ${students}`);
-    console.log(`Number of students in CS: ${studentsCS}. List: ${studentListCS.slice(0, -2)}`);
-    console.log(`Number of students in SWE: ${studentsSWE}. List: ${studentListSWE.slice(0, -2)}`);
-  } catch (err) {
-    throw new Error('Cannot load the database');
   }
+  console.log(`Number of students: ${students}`);
+  console.log(`Number of students in CS: ${studentsCS}. List: ${studentsCSList.join(', ')}`);
+  console.log(`Number of students in SWE: ${studentsSWE}. List: ${studentsSWEList.join(', ')}`);
 }
 
 module.exports = countStudents;
