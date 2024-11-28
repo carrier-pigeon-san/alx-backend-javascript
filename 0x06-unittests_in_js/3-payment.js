30495ad621eaf146924a204f14c2a4ab0f555b5e
Sandy Obare <obaresandy@gmail.com>
2024-11-28 22:48:55 +0300

    3-payment.js update: import modules

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,3 +1,6 @@
+const Utils = require('./utils');
+const calculateNumber = Utils.calculateNumber;
+
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
   const total = calculateNumber('SUM', totalAmount, totalShipping);
   console.log(`The total is: ${total}`);
