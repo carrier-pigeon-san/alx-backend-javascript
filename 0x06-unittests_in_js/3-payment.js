71f6a46af91e3170461437ae0fbfe3c045dc23ca
Sandy Obare <obaresandy@gmail.com>
2024-12-01 23:40:12 +0300

    3-payment.js update: syntax update

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,8 +1,8 @@
 const Utils = require('./utils');
-const calculateNumber = Utils.calculateNumber;
+// const calculateNumber = Utils.calculateNumber;
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
-  const total = calculateNumber('SUM', totalAmount, totalShipping);
+  const total = Utils.calculateNumber('SUM', totalAmount, totalShipping);
   console.log(`The total is: ${total}`);
 }
 
