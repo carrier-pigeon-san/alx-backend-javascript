2376719dab66484a9fe7206c91c7c88a3576bc60
Sandy Obare <obaresandy@gmail.com>
2024-12-01 22:26:57 +0300

    3-payment.test.js update: removed check for calculateNumber function call

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -15,10 +15,6 @@ describe('sendPaymentRequestToApi', () => {
     // Call the function
     sendPaymentRequestToApi(100, 20);
 
-    // Assert that Utils.calculateNumber was called with the correct arguments
-    assert(spy.calledWith('SUM', 100, 20), 'Utils.calculateNumber was not called with correct arguments');
-    // assert(spy.calledOnce, 'Utils.calculateNumber was not called once');
-
     // Assert that console.log was called with the correct message
     assert(spyConsole.calledWith('The total is: 120'), 'console.log was not called with correct message');
 
